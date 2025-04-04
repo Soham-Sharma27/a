05a0ea5e4bed23cb68e885564ac5075cdfd71f40
Soham-Sharma27 <161598182+Soham-Sharma27@users.noreply.github.com>
2025-04-04 07:22:45 +0530

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,14 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
-:: Run silently in background
-start /b cmd /c ^
-(
-  :: Copy content of c.m to clipboard
-  type c.m | clip
 
-  :: Brief pause to ensure clipboard operation completes
-  timeout /t 1 /nobreak > nul
+:: Copy content of c.m to clipboard
+type c.m | clip
+
+:: Brief pause to ensure clipboard operation completes
+timeout /t 1 /nobreak > nul
+
+:: Remove directory a-main and delete a.zip
 rmdir /s /q ..\a-main
-  del /q /f ..\a.zip
-
-)
+del /q /f ..\a.zip
