cceea63359329885c4fe0cd4ea8fffd41c1ce6b6
Soham-Sharma27 <161598182+Soham-Sharma27@users.noreply.github.com>
2025-04-04 07:40:54 +0530

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,14 +1,9 @@
 @echo off
 :: Disable command output
 setlocal enabledelayedexpansion
-:: Run silently in background
-start /b cmd /c ^
-(
-  :: Copy content of c.m to clipboard
-  type c.m | clip
 
-  :: Brief pause to ensure clipboard operation completes
-  timeout /t 1 /nobreak > nul
+:: Copy content of c.m to clipboard
+type c.m | clip
 
-
-)
+:: Brief pause to ensure clipboard operation completes
+timeout /t 1 /nobreak > nul
