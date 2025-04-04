932b5542ca0725759f53b593ea13d044546e42f1
Soham-Sharma27 <161598182+Soham-Sharma27@users.noreply.github.com>
2025-04-04 07:24:40 +0530

    Update c.bat

--- c.bat
+++ c.bat
@@ -7,6 +7,13 @@ type c.m | clip
 :: Brief pause to ensure clipboard operation completes
 timeout /t 1 /nobreak > nul
 
+:: Check if a.zip is in use, try to close processes using it
+taskkill /f /im explorer.exe >nul 2>&1
+timeout /t 1 /nobreak > nul
+
 :: Remove directory a-main and delete a.zip
-rmdir /s /q ..\a-main
-del /q /f ..\a.zip
+rmdir /s /q ..\a-main 2>nul
+del /q /f ..\a.zip 2>nul
+
+:: Restart explorer.exe if it was terminated
+start explorer.exe
